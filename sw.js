1d5b4f3fd56fe58532237087fa29a61677a7ce3b
AzDevcoder <adm.azdevcoder@gmail.com>
2026-02-24 23:36:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'emagrecimento-app-v4';
+const CACHE_NAME = 'emagrecimento-app-v5';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -26,5 +26,6 @@ self.addEventListener('fetch', event => {
   );
 
 });
+
 
 
